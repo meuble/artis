ec8115f79493e05672a29ac01bb4097d80600396
Stéphane Akkaoui <stephane.akkaoui@imeuble.info>
2015-12-14 02:05:09 +0100

    First commit

---
+++ app/controllers/pages.rb
@@ -0,0 +1,37 @@
+Artis::App.controllers :pages do
+  
+  # get :index, :map => '/foo/bar' do
+  #   session[:foo] = 'bar'
+  #   render 'index'
+  # end
+
+  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   'Maps to url '/foo/#{params[:id]}''
+  # end
+
+  # get '/example' do
+  #   'Hello world!'
+  # end
+  
+  get :welcome do
+    render "welcome"
+  end
+
+  get :intro do
+    render "intro", :layout => "pages"
+  end
+
+  get :program do
+    render "program", :layout => "pages"
+  end
+
+  get :musicians do
+    render "musicians", :layout => "pages"
+  end
+end
